a7a5e6977a7a636b601f39f8a3af24d1f75b1d5b
kamaxo <kamaxo93@gmail.com>
2024-10-29 08:43:51 +0100

    Implementation repository provide

--- app/src/main/java/com/camacho/rickandmortyapp/core/di/RepositoryModule.kt
+++ app/src/main/java/com/camacho/rickandmortyapp/core/di/RepositoryModule.kt
@@ -1,12 +1,22 @@
 package com.camacho.rickandmortyapp.core.di
 
+import com.camacho.rickandmortyapp.data.remote.datasorce.RickAndMortyRemoteDataSource
+import com.camacho.rickandmortyapp.data.repository.RickAndMortyRepositoryImpl
+import com.camacho.rickandmortyapp.domain.repository.RickAndMortyRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RepositoryModule {
 
+    @Provides
+    @Singleton
+    fun provideRickAndMortyRepository(dataSource: RickAndMortyRemoteDataSource): RickAndMortyRepository {
+        return RickAndMortyRepositoryImpl(dataSource)
+    }
 
 }
